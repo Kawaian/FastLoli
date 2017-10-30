f1424fcb8d0231f755da392019db77d5265ace4e
AinL <gmlwns5176@gmail.com>
2017-10-30 23:26:49 +0900

    wt

--- src/experiments/FastLoli.Exp/Program.cs
+++ src/experiments/FastLoli.Exp/Program.cs
@@ -10,6 +10,50 @@ namespace FastLoli.Exp
     {
         static void Main(string[] args)
         {
+            Program prg = new Program();
+            prg.Run();
+        }
+
+        class CommandDesc
+        {
+            public string Key { get; set; }
+            public string Describe { get; set; }
+            public CommandDesc(string key, string desc)
+            {
+                Key = key;
+                Describe = desc;
+            }
+        }
+
+        Dictionary<CommandDesc, Action> Commands;
+
+        public Program()
+        {
+            Commands = new Dictionary<CommandDesc, Action>()
+            {
+                {
+                    new CommandDesc("help", "Show helps"),
+                    ()=>
+                    {
+                        foreach (var item in Commands)
+                        {
+                            Console.WriteLine($"{item.Key.Key.PadRight(12)} {item.Key.Describe}");
+	                    }
+                    }
+                },
+                {
+                    new CommandDesc("mnist", "Run CNTK's mnist sample"),
+                    () =>
+                    {
+                        
+                    }
+                }
+            };
+        }
+
+        public void Run()
+        {
+
         }
     }
 }
